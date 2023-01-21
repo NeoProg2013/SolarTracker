5d703f0543ba889d27e0e723136e799aac370add
NeoProg <NeoProg2013@gmail.com>
2023-01-21 18:59:40 +0300

    Fixed HW

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -18,9 +18,69 @@ int main() {
     system_init();
     systimer_init();
     
+    //gpio_set_mode(GPIOA, 9, GPIO_MODE_INPUT);
+    //gpio_set_pull(GPIOA, 9, GPIO_PULL_UP);
+    
+    gpio_reset(GPIOA, 0);
+    gpio_set_mode(GPIOA, 0, GPIO_MODE_INPUT);
+    gpio_set_pull(GPIOA, 0, GPIO_PULL_NO);
+    gpio_reset(GPIOA, 1);
+    gpio_set_mode(GPIOA, 1, GPIO_MODE_INPUT);
+    gpio_set_pull(GPIOA, 1, GPIO_PULL_NO);
+    gpio_reset(GPIOA, 2);
+    gpio_set_mode(GPIOA, 2, GPIO_MODE_INPUT);
+    gpio_set_pull(GPIOA, 2, GPIO_PULL_NO);
+    gpio_reset(GPIOA, 3);
+    gpio_set_mode(GPIOA, 3, GPIO_MODE_INPUT);
+    gpio_set_pull(GPIOA, 3, GPIO_PULL_NO);
+    
+    
+    gpio_reset(GPIOB, 1);
+    gpio_set_mode(GPIOB, 1, GPIO_MODE_OUTPUT);
+    gpio_set_output_type(GPIOB, 1, GPIO_TYPE_PUSH_PULL);
+    
+    gpio_reset(GPIOA, 7);
+    gpio_set_mode(GPIOA, 7, GPIO_MODE_OUTPUT);
+    gpio_set_output_type(GPIOA, 7, GPIO_TYPE_PUSH_PULL);
+    
+    
+    
+    gpio_reset(GPIOA, 6);
+    gpio_set_mode(GPIOA, 6, GPIO_MODE_OUTPUT);
+    gpio_set_output_type(GPIOA, 6, GPIO_TYPE_PUSH_PULL);
+    
+    gpio_reset(GPIOA, 5);
+    gpio_set_mode(GPIOA, 5, GPIO_MODE_OUTPUT);
+    gpio_set_output_type(GPIOA, 5, GPIO_TYPE_PUSH_PULL);
+    
     delay_ms(1000);
     
+    uint64_t start_timeout = get_time_ms();
     while (true) {
+        
+        gpio_reset(GPIOB, 1); // M1
+        gpio_reset(GPIOA, 7); // M1
+        gpio_reset(GPIOA, 6); // M2
+        gpio_reset(GPIOA, 5); // M2
+        delay_ms(100);
+        
+        gpio_set(GPIOB, 1);   // M1
+        gpio_reset(GPIOA, 7); // M1
+        gpio_set(GPIOA, 6);   // M2
+        gpio_reset(GPIOA, 5); // M2
+        delay_ms(10000);
+        
+        gpio_reset(GPIOB, 1); // M1
+        gpio_reset(GPIOA, 7); // M1
+        gpio_reset(GPIOA, 6); // M2
+        gpio_reset(GPIOA, 5); // M2
+        delay_ms(100);
+        
+        gpio_reset(GPIOB, 1); // M1
+        gpio_set(GPIOA, 7);   // M1
+        gpio_reset(GPIOA, 6); // M2
+        gpio_set(GPIOA, 5);   // M2
+        delay_ms(10000);
         
         asm("nop");
     }
@@ -56,7 +116,7 @@ static void system_init(void) {
     while ((RCC->CFGR & RCC_CFGR_SWS_PLL) == 0);*/
 
     // Enable GPIO clocks
-    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN;
     
     // Enable TIM14 clocks
     RCC->APB1ENR |= RCC_APB1ENR_TIM14EN;
